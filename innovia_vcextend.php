f5bac7cf434b85a25c7765b7593218b622d6449d
yeoman <idanishraza@gmail.com>
2016-10-23 20:35:53 +0300

    pushing css changes for testing

--- innovia_vcextend.php
+++ innovia_vcextend.php
@@ -12,7 +12,6 @@
  * @package           Innovia_vcextend
  *
  * @wordpress-plugin
- * GitHub Plugin URI: https://github.com/yeomann/visual-composer-Addons
  * Plugin Name:       Innovia Vcextend
  * Plugin URI:        www.innoviadigital.com
  * Description:       VC extend adon by innovia digital.
@@ -23,6 +22,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       innovia_vcextend
  * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/yeomann/visual-composer-Addons
  */
 
 // If this file is called directly, abort.
